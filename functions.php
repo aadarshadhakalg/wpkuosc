9469136215d18bf862af389695fa006718bdba79
Aadarsha Dhakal <braveboy2058@gmail.com>
2023-03-27 18:56:51 +0545

    Custom Fields

--- functions.php
+++ functions.php
@@ -1,5 +1,45 @@
 <?php
 
+
+// Dependent Plugins
+
+// Dependent plugins
+
+require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
+add_action( 'tgmpa_register', 'install_bundled_plugins' );
+function install_bundled_plugins() {
+
+  $plugins = array(
+    array( 
+      'name'     => 'Advanced Custom Fields',
+      'slug'     => 'advanced-custom-fields', 
+      'source'   => get_template_directory_uri() . '/bundled-plugins/advanced-custom-fields.zip',
+      'required' => true,
+	  'force_activation' => true,
+    ),
+  );
+
+  /*
+   * Array of configuration settings.
+  */
+  $config = array(
+    'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+    'default_path' => '',                      // Default absolute path to bundled plugins.
+    'menu'         => 'tgmpa-install-plugins', // Menu slug.
+    'parent_slug'  => 'themes.php',            // Parent menu slug.
+    'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+    'has_notices'  => true,                    // Show admin notices or not.
+    'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+    'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+    'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+    'message'      => '',                      // Message to output right before the plugins table.
+  
+  );
+  tgmpa( $plugins, $config );
+}
+
+
+
 // Add theme suport
 
 function kuosc_theme_support(){
